29cb7a0352edbac3dc1c26396d52b31029cbb113
Jerson Ramírez Ortiz <juamkoo@gmail.com>
2019-08-31 08:44:12 -0500

    Se agregó la carpeta js Está carpeta contendrá todos los archivos javascript delÃ proyecto por el momento se agregó solo un archivo

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import {saludo, despedida} from "./modules/example"
+import {activeMenu} from './modules/active-menu'
+import {holaMundo} from './modules/hola-mundo'
+
+// saludo()
+// despedida()
+//activeMenu()
+holaMundo();
